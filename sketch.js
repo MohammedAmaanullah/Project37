9473923c08ad17ab7e04f795d99890b09137f100
MohammedAmaanullah <74972725+MohammedAmaanullah@users.noreply.github.com>
2021-03-24 22:56:03 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+var canvas;
+var contestantCount, database, quiz , question, contestant;
+var gameState = 0;
+function setup(){
+  canvas = createCanvas(850,400);
+}
+
+
+function draw(){
+  background("pink");
+
+  
+}
